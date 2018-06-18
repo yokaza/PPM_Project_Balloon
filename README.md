4695105b13cdbe70485e9e9272830af3e527236d
yokaza <37301438+yokaza@users.noreply.github.com>
2018-06-18 11:34:36 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PPM_Project_Balloon
---
+++ app/src/main/java/petra/tugas/ppm_project_balloon/MainActivity.java
@@ -0,0 +1,13 @@
+package petra.tugas.ppm_project_balloon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
